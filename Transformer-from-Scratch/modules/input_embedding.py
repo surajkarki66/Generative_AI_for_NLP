2d9299742be014e08baff53fb1a093cdebcad216
surajkarki <suraj.karki500@protonmail.com>
2024-02-14 18:19:32 +0100

    add input_embedding module

---
+++ Transformer-from-Scratch/modules/input_embedding.py
@@ -0,0 +1,14 @@
+import math
+import torch.nn as nn
+
+class InputEmbeddings(nn.Module):
+
+    def __init__(self, d_model: int, vocab_size: int) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        # (batch, seq_len) --> (batch, seq_len, d_model)
+        return self.embedding(x) * math.sqrt(self.d_model) # Multiply by sqrt(d_model) to scale the embeddings according to the paper
