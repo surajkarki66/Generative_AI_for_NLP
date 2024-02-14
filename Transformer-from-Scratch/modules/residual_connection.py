7a664a630315402d5e3070585aca94797fa09d0b
surajkarki <suraj.karki500@protonmail.com>
2024-02-15 00:15:26 +0100

    finished residual block

---
+++ Transformer-from-Scratch/modules/residual_connection.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+
+from layer_normalization import LayerNormalization
+
+class ResidualConnection(nn.Module):
+    
+        def __init__(self, features: int, dropout: float) -> None:
+            super().__init__()
+            self.dropout = nn.Dropout(dropout)
+            self.norm = LayerNormalization(features)
+    
+        def forward(self, x, sublayer):
+            return x + self.dropout(sublayer(self.norm(x)))
