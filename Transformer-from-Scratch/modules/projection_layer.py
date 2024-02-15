8314f5a1fc492b1dd2563856b5c93750b2fc0455
surajkarki <suraj.karki500@protonmail.com>
2024-02-15 18:20:14 +0100

    finished projection layer?

---
+++ Transformer-from-Scratch/modules/projection_layer.py
@@ -0,0 +1,11 @@
+import torch.nn as nn
+
+class ProjectionLayer(nn.Module):
+
+    def __init__(self, d_model, vocab_size) -> None:
+        super().__init__()
+        self.proj = nn.Linear(d_model, vocab_size)
+
+    def forward(self, x) -> None:
+        # (batch, seq_len, d_model) --> (batch, seq_len, vocab_size)
+        return self.proj(x)
